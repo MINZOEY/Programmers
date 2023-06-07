8ae16cfa9f7a299e52ab0a18ce76c9de89dea887
MINZOEY <132998281+MINZOEY@users.noreply.github.com>
2023-06-07 19:37:59 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,5 @@
 function solution(n, k) {
     var answer = 0;
-    let nw = 12000;
-    let kw = 2000;
-    answer = (n*nw) + (k*kw) - ((Math.floor(n/10))*kw);
+    answer = (n*12000) + (k*2000) - ((Math.floor(n/10))*2000);
     return answer;
 }
